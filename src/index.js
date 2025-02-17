d09e6f8218c21077185dc1249c7ed81aedc42142
David-Cavazos <88981757+David-Cavazos@users.noreply.github.com>
2025-02-17 15:00:31 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Chatbot from './Chatbot';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 
 const App = () => {
@@ -18,4 +19,8 @@ const App = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
